42fdcd72a84a5bbfd9a404d1577a5167585bf3d4
Lawrence Adams <lawrencetadams@googlemail.com>
2017-06-13 20:02:34 +0100

    Get/SetCardQuery working.

---
+++ src/app/common/cards/card.model.ts
@@ -0,0 +1,10 @@
+export class Card {
+    id: number;
+    front: string;
+    back: string;
+    // Originally the below were a part of the attributes model (depricated)
+    year: number;
+    type: string;
+    system: string[];
+    CCP: number[];
+}
