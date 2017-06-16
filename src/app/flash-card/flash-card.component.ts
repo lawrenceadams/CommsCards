df181673d2e7a91263fd08ce98ceee187812db6f
Lawrence Adams <lawrencetadams@googlemail.com>
2017-06-16 15:46:17 +0100

    Flashcard navigation controls now working. Looping over deck implemented

--- src/app/flash-card/flash-card.component.ts
+++ src/app/flash-card/flash-card.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, HostBinding, EventEmitter, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
 import { Subscription } from "rxjs";
 
 import { routeFadeStateTrigger } from "../common/route.animations";
@@ -21,23 +22,29 @@ export class FlashCardComponent implements OnInit, OnDestroy {
   query: string;
   queryterm: string;
   cardsToStudy: Card[];
+  currentCard: Card;
+  currentCardIndex: number;
 
   private sub: any;
+  private currentURL: string;
 
-  constructor(private route: ActivatedRoute, private service: CardProviderService, private messageService: MessageService) {
+  constructor(
+    private route: ActivatedRoute,
+    private service: CardProviderService,
+    private messageService: MessageService,
+    private location: Location
+  ) {
     /*
     * Setup a service listener to recieve each message and act upon it accordingly
     */
     this.subscription = this.messageService.getMessage().subscribe(message => {
       console.info(message);
 
-      if (message === 'next') {
-        this.nextCard()
-      } else if (message === 'previous') {
+      if (message.text === 'next') {
+        this.nextCard();
+      } else if (message.text === 'previous') {
         this.previousCard();
       }
-
-
     });
   }
 
@@ -46,9 +53,11 @@ export class FlashCardComponent implements OnInit, OnDestroy {
     this.sub = this.route.params.subscribe(params => {
       this.query = params['query']; // Get the 'queryby' (system/type/year) from the url
       this.queryterm = params['queryterm']; // Get the 'query' (year 1/2/3) from the url. Bit hacky ~ might need to replace at somepoint
+      this.currentCardIndex = params['id']; // Sets the current card index from the URL.
     });
 
     this.cardsToStudy = this.service.getCards();
+    this.currentURL = this.location.path();
 
     if (this.cardsToStudy.length === 0) {
       console.warn("Route was hotlinked! No current card state is set.");
@@ -65,12 +74,50 @@ export class FlashCardComponent implements OnInit, OnDestroy {
     this.subscription.unsubscribe();
   }
 
+  /**
+   * Checks if end of index or not, if so then set currentCardIndex to 0
+   * If active card is last in stack of cards, start again over deck
+   * Otherwise, increase activeCardID
+   */
   nextCard() {
-    // TODO
+    if (this.cardsToStudy.length === (this.currentCardIndex + 1)) {
+      this.currentCardIndex = 0;
+      // TODO Notify user that we are looping
+    } else {
+      this.currentCardIndex++;
+    }
+
+    this.updateURLPath();
   }
 
+  /**
+   * Check if beginning of index or not, if so then set currentCardIndex to end of cardsToStudy array
+   * If active card is first in stack of cards, goto end of deck
+   * Otherwise, decrease activeCardID
+   */
   previousCard() {
-    // TODO
+    if (this.currentCardIndex === 0) {
+      this.currentCardIndex = this.cardsToStudy.length - 1;
+      // TODO Notify user that we are looping back.
+      console.info("At beginning of deck - Looping back to end.")
+    } else {
+      this.currentCardIndex--;
+    }
+    this.updateURLPath();
   }
 
+  /**
+   * Used to update the URL for ease of use and allowing of sharing of cards.
+   * 
+   * Gets the current url, splits it into an array of string, deletes the last item in array
+   * then adds the new card index and sets the URL bar to this string
+   */
+  updateURLPath() {
+    let strippedURL = this.currentURL.split('/');
+    strippedURL.pop();
+    strippedURL.push(String(this.currentCardIndex));
+
+    this.currentURL = strippedURL.join('/');
+    this.location.go(this.currentURL);
+  }
 }
