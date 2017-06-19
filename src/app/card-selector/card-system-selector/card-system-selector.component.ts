e3cb521bc1512295d6140380eb91b3e999d7c2cc
Lawrence Adams <lawrencetadams@googlemail.com>
2017-06-19 12:43:40 +0100

    Added all System Options to /study/system page
    Also corrected some cards

--- src/app/card-selector/card-system-selector/card-system-selector.component.ts
+++ src/app/card-selector/card-system-selector/card-system-selector.component.ts
@@ -21,6 +21,21 @@ export class CardSystemSelectorComponent implements OnInit {
     new MenuOption("Muskuloskeletal", "/study/system/msk"),
     new MenuOption("Endocrine", "/study/system/endo"),
     new MenuOption("Child and Family", "/study/system/paeds"),
+    new MenuOption("Ophthalmology", "/study/system/ophthal"),
+    new MenuOption("ENT", "/study/system/ent"),
+    new MenuOption("Renal", "/study/system/renal"),
+    new MenuOption("Urology", "/study/system/urol"),
+    new MenuOption("Ageing", "/study/system/age"),
+    new MenuOption("Neurology", "/study/system/neuro"),
+    new MenuOption("Psychiatry", "/study/system/psych"),
+    new MenuOption("Obstetrics and Gynaecology", "/study/system/obgyn"),
+    new MenuOption("Haematology", "/study/system/haem"),
+    new MenuOption("Microbiology", "/study/system/microbio"),
+    new MenuOption("Pharmacology", "/study/system/pharma"),
+    new MenuOption("Ethics and Law", "/study/system/ethlaw"),
+    new MenuOption("General Practice", "/study/system/gp"),
+    new MenuOption("Public Health", "/study/system/pubhealth"),
+    new MenuOption("Radiology", "/study/system/radiol")
   ]
 
   constructor() { }
