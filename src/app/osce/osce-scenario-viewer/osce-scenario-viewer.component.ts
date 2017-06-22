e471ea5fa6dbdd7c94058bda24526e2a4806cd71
Lawrence Adams <lawrencetadams@googlemail.com>
2017-06-22 19:54:48 +0100

    Added animations and first scenario to ScenarioStore

--- src/app/osce/osce-scenario-viewer/osce-scenario-viewer.component.ts
+++ src/app/osce/osce-scenario-viewer/osce-scenario-viewer.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostBinding } from '@angular/core';
 import { ActivatedRoute } from "@angular/router";
 import { OSCE_SCENARIOS } from "../osceScenarios.store";
+import { routeFadeStateTrigger } from "../../common/route.animations";
 
 @Component({
   selector: 'app-osce-scenario-viewer',
   templateUrl: './osce-scenario-viewer.component.html',
-  styleUrls: ['./osce-scenario-viewer.component.css']
+  styleUrls: ['./osce-scenario-viewer.component.css'],
+  animations: [routeFadeStateTrigger]
 })
 export class OsceScenarioViewerComponent implements OnInit {
+  @HostBinding('@routeFadeState') routeAnimation = true;
 
   private sub: any;
   id: number;
